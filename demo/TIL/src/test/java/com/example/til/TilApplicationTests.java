6d9c0a02770cd9b0d04158c544f8e4f542a98dd5
손승범 <dhsmf4245@gmail.com>
2026-07-16 13:04:13 +0900

    Initial commit

---
+++ demo/TIL/src/test/java/com/example/til/TilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.til;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
