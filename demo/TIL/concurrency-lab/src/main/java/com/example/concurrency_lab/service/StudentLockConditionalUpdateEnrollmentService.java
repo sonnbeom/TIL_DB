789cf62a0b7571d89a5099ee8ad4d96e1b7eae40
sonnbeom <dhsmf4245@gmail.com>
2026-07-28 15:54:00 +0900

    TIL

--- demo/TIL/concurrency-lab/src/main/java/com/example/concurrency_lab/service/StudentLockConditionalUpdateEnrollmentService.java
+++ demo/TIL/concurrency-lab/src/main/java/com/example/concurrency_lab/service/StudentLockConditionalUpdateEnrollmentService.java
@@ -1,15 +1,20 @@
 package com.example.concurrency_lab.service;
 
 import com.example.concurrency_lab.domain.Course;
+import com.example.concurrency_lab.domain.CourseTimeSlot;
+import com.example.concurrency_lab.domain.Enrollment;
 import com.example.concurrency_lab.dto.EnrollmentRequest;
 import com.example.concurrency_lab.dto.EnrollmentResult;
 import com.example.concurrency_lab.exception.CapacityExceededException;
 import com.example.concurrency_lab.repository.CourseRepository;
+import com.example.concurrency_lab.repository.CourseTimeSlotRepository;
 import com.example.concurrency_lab.repository.EnrollmentRepository;
 import com.example.concurrency_lab.repository.StudentRepository;
 import com.example.concurrency_lab.validator.EnrollmentValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class StudentLockConditionalUpdateEnrollmentService implements Enrollment
     private final CourseRepository courseRepository;
     private final EnrollmentValidator enrollmentValidator;
     private final EnrollmentRepository enrollmentRepository;
+    private final CourseTimeSlotRepository courseTimeSlotRepository;
 
     @Override
     public EnrollmentResult enroll(EnrollmentRequest enrollmentRequest) {
@@ -40,7 +46,18 @@ public class StudentLockConditionalUpdateEnrollmentService implements Enrollment
                 throw new CapacityExceededException(
                         String.format("강의 id(%d)를 가진 정원이 마감되었습니다.", courseId));
             }
-            enrollmentRepository.save();
+            enrollmentRepository.save(
+                    Enrollment.builder()
+                            .studentId(studentId)
+                            .courseId(courseId)
+                            .build()
+            );
+
+            List<CourseTimeSlot> slots = courseTimeSlotRepository.findByCourseId(courseId);
+
+            for(CourseTimeSlot courseTimeSlot : slots){
+
+            }
         }
         catch (RuntimeException e) {
             return EnrollmentResult.builder()
