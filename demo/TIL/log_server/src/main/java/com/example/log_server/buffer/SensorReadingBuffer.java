34a0e05e0efb166987574d3cb210c9e7396ea864
sonnbeom <dhsmf4245@gmail.com>
2026-08-01 12:00:00 +0900

    TIL

---
+++ demo/TIL/log_server/src/main/java/com/example/log_server/buffer/SensorReadingBuffer.java
@@ -0,0 +1,11 @@
+package com.example.log_server.buffer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SensorReadingBuffer {
+}
