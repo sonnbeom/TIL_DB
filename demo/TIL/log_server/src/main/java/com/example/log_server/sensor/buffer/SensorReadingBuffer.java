4ac9b00b65bfe3283b28a5a0c7f033f6f3910980
sonnbeom <dhsmf4245@gmail.com>
2026-08-02 12:00:00 +0900

    TIL

--- demo/TIL/log_server/src/main/java/com/example/log_server/sensor/buffer/SensorReadingBuffer.java
+++ demo/TIL/log_server/src/main/java/com/example/log_server/sensor/buffer/SensorReadingBuffer.java
@@ -1,15 +1,23 @@
 package com.example.log_server.sensor.buffer;
 
+import com.example.log_server.sensor.domain.SensorReading;
+import com.example.log_server.sensor.writer.SensorReadingWriter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SensorReadingBuffer {
-    private final MongoTemplate mongoTemplate;
 
+    private final SensorReadingWriter writer;
     private static final int BATCH_SIZE = 200;
+
+    private final BlockingQueue<SensorReading> buffer = new LinkedBlockingQueue<>();
+
 }
