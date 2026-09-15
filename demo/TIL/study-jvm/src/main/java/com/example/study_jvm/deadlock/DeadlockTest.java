ec8d811c1c48b79ac15952a2166270efdc93e1d2
sonnbeom <dhsmf4245@gmail.com>
2026-09-15 18:00:27 +0900

    TIKL

--- demo/TIL/study-jvm/src/main/java/com/example/study_jvm/deadlock/DeadlockTest.java
+++ demo/TIL/study-jvm/src/main/java/com/example/study_jvm/deadlock/DeadlockTest.java
@@ -1,5 +1,12 @@
 package com.example.study_jvm.deadlock;
 
 public class DeadlockTest {
-    private static final Object
+    private static final Object lockA = new Object();
+    private static final Object lockB = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+
+        });
+    }
 }
